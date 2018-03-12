f2805180ffc1f861cb10970402079774ea946157
Alexander Urizar <urizaraj@gmail.com>
2018-03-12 16:51:55 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "students", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.boolean  "active",     default: true
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
